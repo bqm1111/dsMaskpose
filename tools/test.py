96dadf97d66d5fe38d6e3acb5d61a8f38590c6ad
bqm1111 <buiquangminh1111@gmail.com>
2023-02-16 17:32:49 +0700

    add code to generate .onnx

--- tools/test.py
+++ tools/test.py
@@ -1,16 +1,14 @@
 import tensorrt as trt
 from maskpose.maskpose import MaskPose
 from maskpose.models import MergeResNet
-from maskpose.models import load_filtered_state_dict
+from maskpose.maskpose import load_filtered_state_dict
 from torchvision.models.resnet import BasicBlock
 import os
 import torch
 import numpy as np
 import torch.onnx
 
-def preprocess(
-    img: np.ndarray, mean: float = 127.5, std: float = 128.0, device="cuda:0"
-) -> torch.Tensor:
+def preprocess(img, mean = 127.5, std = 128.0, device="cuda:0"):
     """preprocess image for inference"""
     img = img.transpose((2, 0, 1))
     img = img.astype(np.float32)
@@ -24,7 +22,7 @@ def softmax_temperature(tensor, temperature):
     result = torch.exp(tensor / temperature)
     result = torch.div(result, torch.sum(result, 1).unsqueeze(1).expand_as(result))
     return result
-
+# 
 if __name__ == '__main__':
     model = MergeResNet(BasicBlock, [2, 2, 2, 2])
     snapshot = os.path.join(os.path.dirname(__file__),
@@ -34,4 +32,4 @@ if __name__ == '__main__':
     model.eval()
     
     dummy_input = torch.randn(1, 3, 112, 112)
-    torch.onnx.export(model, dummy_input, "maskpose.onnx", input_names="face_input", output_names="maskpose_vector", verbose=True, export_params=True)
+    torch.onnx.export(model, dummy_input, "maskpose.onnx", input_names="face_input", output_names=["mask", "yaw", "pitch", "roll"], verbose=True, export_params=True)
